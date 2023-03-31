49eaf7b765bd3daef4c3e70aecbbb603f9863c8d
stephanroberts <stephanrobert@posteo.de>
2023-03-31 23:20:02 +0200

    Implemented the basics for the state machine

---
+++ 3rdPersonCombat/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    [field: SerializeField] public InputReader InputReader { get; private set; }
+    private void Start() {
+        SwitchState(new PlayerTestState(this));
+    }   
+
+}
---
+++ 3rdPersonCombat/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    private float timer;
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){}
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
+    }
+    public override void Tick(float deltaTime)
+    {
+        timer += deltaTime;
+        Debug.Log(timer);
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump() {
+        Debug.Log("Jump");
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
+    }
+
+}
